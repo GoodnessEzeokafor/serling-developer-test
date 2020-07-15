fec441a8b94f72d9f1b60e7383fa058bbff871af
Goodness Ezeokafor <gootech442@yahoo.com>
2020-07-15 14:33:20 +0100

    Developer:Goodness Ezeokafor Message:Working on auth

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,6 +2,6 @@ module.exports = function(req, res,next){
 
     // 401 unauthorized
     // 403 Forbidden
-    if (!req.user.isAdmin) return res.status(403).send('Access denied.');
+    if (!req.user.isAdmin) return res.status(403).send('Access denied. NOT AN ADMIN');
     next()
 }
--- routes/api/admin.js
+++ routes/api/admin.js
@@ -1,5 +1,58 @@
 const express = require("express");
 const router = express.Router();
+const {User} = require("../../models/User")
+const admin = require("../../middleware/admin")
+const auth = require("../../middleware/auth")
+
+/** set admin **/
+router.put("/set-admin/:id",auth,async (req, res) => {
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, {$set:{
+            'isAdmin':true
+        }})
+        res.json(user)
+    }catch(e){
+        return res.status(400).json(e.message); 
+    }
+})
 
 
+/** set admin **/
+
+
+/** GET ALL USERS */
+router.get("/get-users", admin,async(req, res) => {
+    try{
+        const user = await User.find()
+        res.json(user)
+    }catch(e){
+        return res.status(400).json(e.message); 
+    }
+})
+/** GET ALL USERS */
+
+
+/** CREATE TEAM  */
+
+/** CREATE TEAM  */
+
+
+/** EDIT TEAM  */
+/** EDIT TEAM */
+
+/** DELETE TEAM  */
+
+/** DELETE TEAM  */
+
+
+/** CREATE FIXTURES */
+/** CREATE FIXTURES */
+
+
+/** EDIT FIXTURES */
+/** EDIT FIXTURES */
+
+/** REMOVE FIXTURES */
+
+/** REMOVE FIXTURES */
 module.exports = router;
