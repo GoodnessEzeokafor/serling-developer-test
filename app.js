d2583bd7ff3fdbc2d80ae1795b146220baba5a7d
Goodness Ezeokafor <gootech442@yahoo.com>
2020-07-16 21:22:34 +0100

    heroku

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ if ( app.get('env') === 'development' ) {
     .catch(err => console.log(err));  
 }else{
   console.log("PRODUCTION .....")
-  mongoose.connect('mongodb+srv://sterling:sterling@cluster0.bo0wv.mongodb.net/sterling?retryWrites=true&w=majority',
+  mongoose.connect('mongodb+srv://sterling:E4T2M5Y3b1ZNT1SP@cluster0.bo0wv.mongodb.net/sterling?retryWrites=true&w=majority',
     { 
         useNewUrlParser: true,
         useUnifiedTopology: true,
